a639ccaf792e1f0a268607360bee2365d9070e97
kulovvoj <vojtech.kulovany@epptec.eu>
2021-06-16 16:33:26 +0200

    Added 4th point (Map of people with first name as key)

--- src/eu/epptec/lambda/Person.java
+++ src/eu/epptec/lambda/Person.java
@@ -1,12 +1,40 @@
 package eu.epptec.lambda;
 
 public class Person {
-    String firstName, lastName;
-    int age;
+    private String firstName, lastName;
+    private int age;
 
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName + " - " + age;
     }
 }
