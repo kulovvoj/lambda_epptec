db87c29533edfdb0f486836d7d55be60d81eb833
kulovvoj <vojtech.kulovany@epptec.eu>
2021-06-17 09:48:56 +0200

    Added 6th point, custom sort

--- src/eu/epptec/lambda/Lambda.java
+++ src/eu/epptec/lambda/Lambda.java
@@ -1,6 +1,7 @@
 package eu.epptec.lambda;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,7 +10,7 @@ import static java.util.Arrays.stream;
 
 public class Lambda {
     public static void main(String[] args) {
-        Person[] people = {
+        List<Person> people = Arrays.asList(
                 new Person("Vojtech", "Kulovany", 24),
                 new Person("Zbysek", "Pantucek", 53),
                 new Person("Tomas", "Holy", 22),
@@ -19,44 +20,48 @@ public class Lambda {
                 new Person("Art", "Garfunkel", 79),
                 new Person("Tomas", "Masaryk", 87),
                 new Person("Zdenek", "Kulovany", 63)
-        };
+        );
 
         // --------------------------------------------
         // 1) Count the number of people named "Tomas"
         // --------------------------------------------
-        long tomasCount = stream(people).filter(person -> person.getFirstName().equals("Tomas")).count();
+        long tomasCount = people
+                .stream()
+                .filter(person -> person.getFirstName().equals("Tomas"))
+                .count();
+
         System.out.println(tomasCount);
         System.out.println();
 
         // --------------------------------------------
         // 2) Get an array of unique last names
         // --------------------------------------------
-        String[] uniqueLastNames = stream(people).map(Person::getLastName).distinct().toArray(String[]::new);
-        System.out.println(Arrays.toString(uniqueLastNames));
+        List<String> uniqueLastNames = people
+                .stream()
+                .map(Person::getLastName)
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println(uniqueLastNames);
         System.out.println();
 
         // --------------------------------------------
         // 3) Count the sum of people's age
         // --------------------------------------------
-        int ageSum = stream(people).map(Person::getAge).reduce(0, (acc, age) -> acc + age);
+        int ageSum = people
+                .stream()
+                .map(Person::getAge)
+                .reduce(0, (acc, age) -> acc + age);
+
         System.out.println(ageSum);
         System.out.println();
 
         // --------------------------------------------
         // 4) Group people to map by name
         // --------------------------------------------
-
-        // First solution
-        /* Map<String, List<Person>> firstNameMap
-                = stream(people).collect(
-                        Collectors.toMap(Person::getFirstName,
-                            person -> Stream.of(person).collect(Collectors.toList()),
-                            (existing, replacement)
-                                    -> Stream.concat(existing.stream(), replacement.stream()).collect(Collectors.toList()))); */
-
-        // Second solution
-        Map<String, List<Person>> firstNameMap
-                = stream(people).collect(Collectors.groupingBy(Person::getFirstName));
+        Map<String, List<Person>> firstNameMap = people
+                .stream()
+                .collect(Collectors.groupingBy(Person::getFirstName));
 
         firstNameMap.forEach((name, personList) -> System.out.println(name + " -> " + personList.toString()));
         System.out.println();
@@ -66,13 +71,27 @@ public class Lambda {
         // by first name first and last name second
         // --------------------------------------------
 
-        Map<String, Map<String, List<Person>>> fullNameMap
-                = stream(people).collect(Collectors.groupingBy(Person::getFirstName, Collectors.groupingBy(Person::getLastName)));
+        Map<String, Map<String, List<Person>>> fullNameMap = people
+                .stream()
+                .collect(Collectors.groupingBy(Person::getFirstName, Collectors.groupingBy(Person::getLastName)));
 
-        fullNameMap.forEach((firstName, lastNameMap)
-                -> { System.out.println(firstName);
+        fullNameMap.forEach(
+                (firstName, lastNameMap) -> {
+                    System.out.println(firstName);
                     lastNameMap.forEach((lastName, personList)
-                    -> System.out.println(" -> " + lastName + " -> " + personList.toString()));});
+                            -> System.out.println(" -> " + lastName + " -> " + personList.toString()));
+                });
+        System.out.println();
 
+        // 6) Sort the people by last name first, first name second
+        Comparator<Person> nameComparator = Comparator.comparing(Person::getLastName).thenComparing(Person::getFirstName);
+        List<Person> sortedPeople = people
+                .stream()
+                .sorted(nameComparator)
+                .collect(Collectors.toList());
+
+        System.out.println(sortedPeople);
+
+        
     }
 }
