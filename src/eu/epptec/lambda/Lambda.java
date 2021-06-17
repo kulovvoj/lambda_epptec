68c606dd2c8d3d1ce7322694ff73aec6577f4895
kulovvoj <vojtech.kulovany@epptec.eu>
2021-06-17 11:02:50 +0200

    Created custom collector and used it in point 2

--- src/eu/epptec/lambda/Lambda.java
+++ src/eu/epptec/lambda/Lambda.java
@@ -1,14 +1,43 @@
 package eu.epptec.lambda;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
-import static java.util.Arrays.stream;
+import java.util.stream.Stream;
 
 public class Lambda {
+    private static class UniqueLastNameCollector implements Collector<Person, Set<String>, List<String>> {
+
+        @Override
+        public Supplier<Set<String>> supplier() {
+            return HashSet::new;
+        }
+
+        @Override
+        public BiConsumer<Set<String>, Person> accumulator() {
+            return (lastNameList, person) -> lastNameList.add(person.getLastName());
+        }
+
+        @Override
+        public BinaryOperator<Set<String>> combiner() {
+            return (left, right) -> Stream.concat(left.stream(), right.stream()).collect(Collectors.toSet());
+        }
+
+        @Override
+        public Function<Set<String>, List<String>> finisher() {
+            return accList -> accList.stream().toList();
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Set.of(Characteristics.UNORDERED);
+        }
+    }
+
     public static void main(String[] args) {
         List<Person> people = Arrays.asList(
                 new Person("Vojtech", "Kulovany", 24),
@@ -34,13 +63,20 @@ public class Lambda {
         System.out.println();
 
         // --------------------------------------------
-        // 2) Get an array of unique last names
+        // 2) Get a list of unique last names
         // --------------------------------------------
+
+        // First solution
         List<String> uniqueLastNames = people
                 .stream()
                 .map(Person::getLastName)
                 .distinct()
                 .collect(Collectors.toList());
+
+        // Solution using custom collector
+        uniqueLastNames = people
+                .stream()
+                .collect(new UniqueLastNameCollector());
 
         System.out.println(uniqueLastNames);
         System.out.println();
@@ -48,10 +84,19 @@ public class Lambda {
         // --------------------------------------------
         // 3) Count the sum of people's age
         // --------------------------------------------
+
+        // First solution
+
         int ageSum = people
                 .stream()
                 .map(Person::getAge)
                 .reduce(0, (acc, age) -> acc + age);
+
+        // Second solution
+
+        ageSum = people
+                .stream()
+                .collect(Collectors.summingInt(Person::getAge));
 
         System.out.println(ageSum);
         System.out.println();
@@ -92,6 +137,6 @@ public class Lambda {
 
         System.out.println(sortedPeople);
 
-        
+
     }
 }
